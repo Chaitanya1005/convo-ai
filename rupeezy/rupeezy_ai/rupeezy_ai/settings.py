7d84ff11e0082934b82c3a393b0aa8aee97f9b5d
Chaitanya 1005 <1005thakkar@gmail.com>
2026-05-08 00:02:04 +0530

    vsv

--- rupeezy/rupeezy_ai/rupeezy_ai/settings.py
+++ rupeezy/rupeezy_ai/rupeezy_ai/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheader',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
